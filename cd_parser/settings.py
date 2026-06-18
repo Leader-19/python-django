40eddbab72fe69bbd2cafa01872d8ce05cc6dd0f
Leader-19 <dinleader200@gmail.com>
2026-06-18 16:12:52 +0700

    clean project structure

--- cd_parser/settings.py
+++ cd_parser/settings.py
@@ -79,7 +79,7 @@ import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
     )
 }
 
